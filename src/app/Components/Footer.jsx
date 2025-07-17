33a91d5d0cb388cf147ad038f388893fb02705fc
islamemon59 <islamemon59@gmail.com>
2025-07-17 23:37:18 +0600

    condinally navbar footer added

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,11 +1,18 @@
+"use client";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Footer = () => {
-  return (
-    <footer className="w-full text-center bg-blue-400 py-4">
-      <p>Copyright 2025</p>
-    </footer>
-  );
+  const pathRouter = usePathname();
+  if (pathRouter.includes == "dashboard") {
+    return (
+      <footer className="w-full text-center bg-blue-400 py-4">
+        <p>Copyright 2025</p>
+      </footer>
+    );
+  } else {
+    <></>;
+  }
 };
 
 export default Footer;
--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,35 +1,43 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <div>
-      <nav className="w-full py-6 bg-blue-400 flex justify-between px-10">
-        <ul className="flex justify-center gap-6 items-center font-bold text-white">
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-          <Link href="/contact">
-            <li>Contact</li>
-          </Link>
-          <Link href="/services">
-            <li>Services</li>
-          </Link>
-        </ul>
-        <div>
-          <Link href="/login" className="px-4 py-2 rounded border">
-            Login
-          </Link>
-          <Link href="/register" className="px-4 py-2 rounded border ml-2">
-            Register
-          </Link>
-        </div>
-      </nav>
-    </div>
-  );
+  const pathRouter = usePathname();
+
+  if (!pathRouter.includes("dashboard")) {
+    return (
+      <div>
+        <nav className="w-full py-6 bg-blue-400 flex justify-between px-10">
+          <ul className="flex justify-center gap-6 items-center font-bold text-white">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/about">
+              <li>About</li>
+            </Link>
+            <Link href="/contact">
+              <li>Contact</li>
+            </Link>
+            <Link href="/services">
+              <li>Services</li>
+            </Link>
+          </ul>
+          <div>
+            <Link href="/login" className="px-4 py-2 rounded border">
+              Login
+            </Link>
+            <Link href="/register" className="px-4 py-2 rounded border ml-2">
+              Register
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  } else {
+    <></>;
+  }
 };
 
 export default Navbar;
