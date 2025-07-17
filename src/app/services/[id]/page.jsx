616c6517377b76620906ce05e44438825232c22b
islamemon59 <islamemon59@gmail.com>
2025-07-17 22:52:03 +0600

    static dynamic route dynamic service details added

---
+++ src/app/services/[id]/page.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const ServiceDetails = ({ params }) => {
+  const id = params.id;
+
+  const data = [
+    {
+      id: "u1",
+      name: "Rafiul Islam",
+      image: "https://randomuser.me/api/portraits/men/32.jpg",
+    },
+    {
+      id: "u2",
+      name: "Nusrat Jahan",
+      image: "https://randomuser.me/api/portraits/women/44.jpg",
+    },
+    {
+      id: "u3",
+      name: "Tanvir Hasan",
+      image: "https://randomuser.me/api/portraits/men/65.jpg",
+    },
+    {
+      id: "u4",
+      name: "Mim Akter",
+      image: "https://randomuser.me/api/portraits/women/21.jpg",
+    },
+    {
+      id: "u5",
+      name: "Sajid Mahmud",
+      image: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+  ];
+
+  const singleData = data.find(d => d.id == id)
+
+  return (
+    <div>
+      <p>ID: {id}</p>
+      <p>{singleData.name}</p>
+      <img src={singleData.image} alt="" />
+    </div>
+  );
+};
+
+export default ServiceDetails;
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -7,6 +7,34 @@ const Services = () => {
   const router = useRouter();
   const isTrue = false;
 
+  const data = [
+    {
+      id: "u1",
+      name: "Rafiul Islam",
+      image: "https://randomuser.me/api/portraits/men/32.jpg",
+    },
+    {
+      id: "u2",
+      name: "Nusrat Jahan",
+      image: "https://randomuser.me/api/portraits/women/44.jpg",
+    },
+    {
+      id: "u3",
+      name: "Tanvir Hasan",
+      image: "https://randomuser.me/api/portraits/men/65.jpg",
+    },
+    {
+      id: "u4",
+      name: "Mim Akter",
+      image: "https://randomuser.me/api/portraits/women/21.jpg",
+    },
+    {
+      id: "u5",
+      name: "Sajid Mahmud",
+      image: "https://randomuser.me/api/portraits/men/12.jpg",
+    },
+  ];
+
   const handleServiceTrue = () => {
     if (isTrue) {
       router.push("/services/address");
@@ -28,6 +56,19 @@ const Services = () => {
       >
         Address
       </button>
+
+      <div>
+        {data.map((d) => {
+          return (
+            <div key={d.id} className="space-y-4">
+              <p>{d.name}</p>
+              <Link href={`/services/${d.id}`}>
+                <img src={d.image} alt="" />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
